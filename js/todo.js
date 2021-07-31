21d4c0609ee1955f424a235b729ffd94c980056d
gihyeonee <82225716+gihyeonee@users.noreply.github.com>
2021-07-31 23:34:18 +0900

    the end..
    finish.....

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,63 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+  // JSON.stringify localStorage에 있는 것들을 string으로 바꿔줌.
+}
+
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
+}
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  li.id = newTodo.id;
+  const span = document.createElement("span");
+  span.innerText = newTodo.text;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  // window + "." 을 누르면, 이모지 찾을 수 있음.
+  button.addEventListener("click", deleteToDo);
+
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  // JSON.parse()는 ()안에 있는 것을 array로 바꿔줌.
+
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+
+  // parsedToDos.forEach((item) => console.log("this is the turn of ", item));
+  // array 안의   각각에 대해서 function을 실행하게 해 줌.
+  // (인자) => console.log();
+  // 처럼 함수를 짧게 쓸 수 있음. arrow function이라고 함.
+}
